0677f12a25e2c3dca2be22c42e2018048a31df89
Gustavo Dev <gustavopicolitona@gmail.com>
2024-03-22 14:14:22 -0300

    New solution / Loop Structure

---
+++ Loop Structures/exercise_1.py
@@ -0,0 +1,14 @@
+'''
+Faça um programa que peça uma nota, entre zero e dez. 
+Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+
+'''
+
+while True:
+    nota = float(input("Digite uma nota entre zero e dez: "))
+    
+    if 0 <= nota <= 10:
+        print("Nota válida:", nota)
+        break
+    else:
+        print("Nota inválida. Por favor, digite novamente.")
