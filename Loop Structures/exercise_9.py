8a82b862a9b4c9cd07fe54b7290659431acb2871
Gustavo Dev <gustavopicolitona@gmail.com>
2024-03-23 12:13:50 -0300

    New solution / Loop Structure

---
+++ Loop Structures/exercise_9.py
@@ -0,0 +1,16 @@
+'''
+Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+'''
+
+for num in range(1, 50 + 1): # +1 para contar o 50
+    # Verifica se o número é ímpar
+    if num % 2 != 0: # se o %(resto) de 2 for diferente de 0 entao é impar
+        print(num) 
+
+# Imprime a quantidade de valores pares
+
+
+
+
+
+
