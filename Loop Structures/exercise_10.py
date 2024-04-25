4fa8bde086fc777d61b832e6b75f13bf9f776928
Gustavo Dev <gustavopicolitona@gmail.com>
2024-04-25 14:52:23 -0300

    New solution / Loop Structure

--- Loop Structures/exercise_10.py
+++ Loop Structures/exercise_10.py
@@ -2,16 +2,16 @@
 Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
 '''
 
+contador_impares = 0  # Inicializa o contador de números ímpares
 
-a = int(input('Número A:'))
-b = int(input('Número B:'))
+for num in range(1, 51):  # Loop de 1 a 50
+    # Verifica se o número é ímpar
+    if num % 2 != 0:
+        print(num)  # Se for ímpar, imprime o número
+        contador_impares += 1  # Incrementa o contador de números ímpares
 
-for num in range(a, b + 1): # para contar o ultimo numero do B
-    # Verifica se o número é ímpar # se o %(resto) de 2 for diferente de 0 entao é impar
-        print(num) 
-
-# Imprime a quantidade de valores pares
-
+# Imprime a quantidade de valores ímpares
+print("A quantidade de números ímpares entre 1 e 50 é:", contador_impares)
 
 
 
